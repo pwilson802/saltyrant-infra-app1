4f88213199ecd1a7840a22a08f7e2a6c39622b08
Paul Wilson <pwilson802@gmail.com>
2025-08-26 04:02:06 +0000

    added readonly output

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -1,4 +1,5 @@
 output "state_bucket" { value = aws_s3_bucket.state.bucket }
 output "state_bucket_arn" { value = aws_s3_bucket.state.arn }
 output "role_arn" { value = aws_iam_role.tf_gh.arn }
+output "role_readonly_arn" { value = aws_iam_role.tf_gh_readonly.arn }
 output "kms_key_arn" { value = try(aws_kms_key.tf[0].arn, null) }
